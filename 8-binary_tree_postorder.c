62e01ba09a68e356ff9a2162b420ece072531d7c
tizihoxha <tizihoxha@gmail.com>
2022-08-31 06:01:16 -0700

    betty

--- 8-binary_tree_postorder.c
+++ 8-binary_tree_postorder.c
@@ -1,10 +1,11 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
+ * binary_tree_postorder - Function that goes through a binary tree using
+ * post-order traversal
+ * @tree: is a pointer to the root node of the tree to traverse
+ * @func: is a pointer to a function to call for each node
+ * Return: no return
  */
 
 void binary_tree_postorder(const binary_tree_t *tree, void (*func)(int))
