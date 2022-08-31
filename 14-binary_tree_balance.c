58402a5d2a50b951fdc32b55a307447e5fc776fe
eno007 <eno.gupe@gmail.com>
2022-08-31 15:34:28 -0700

    update betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,7 +2,7 @@
 /**
  * binary_tree_height_1 - measures the height of the tree
  * @tree: pointer to the root node of the tree
- * Return : Balance factor or 0
+ * Return: Balance factor or 0
  */
 int binary_tree_height_1(const binary_tree_t *tree)
 {
@@ -20,12 +20,12 @@ int binary_tree_height_1(const binary_tree_t *tree)
 /**
  * binary_tree_balance - measures the balance of the tree
  * @tree: pointer to the root node of the tree
- * Return : Balance factor or 0
+ * Return: Balance factor or 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
 	int Rheight, Lheight;
-	
+
 	if (!tree)
 		return (0);
 	Rheight = binary_tree_height_1(tree->right);
