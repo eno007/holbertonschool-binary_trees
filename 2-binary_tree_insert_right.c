3da85a49ba274746e3c4457dbde44752a8ac78de
tizihoxha <tizihoxha@gmail.com>
2022-08-31 05:10:44 -0700

    fixed case

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -23,7 +23,7 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 	new_node->n = value;
 	new_node->left = NULL;
 	new_node->right = NULL;
-	if (parent->left == NULL)
+	if (parent->right == NULL)
 	{
 		parent->right = new_node;
 		new_node->parent = parent;
