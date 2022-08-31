11dca536857989a929b2bf90d0b912caff28b27f
tizihoxha <tizihoxha@gmail.com>
2022-08-31 03:47:44 -0700

    added task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -28,7 +28,7 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 		parent->left = new_node;
 		new_node->parent = parent;
 	}
-	else 
+	else
 	{
 		tmp = parent->left;
 		parent->left = new_node;
