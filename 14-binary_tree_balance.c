e636cc1395ccc78749100324351565201aaa5504
eno007 <eno.gupe@gmail.com>
2022-08-31 15:31:08 -0700

    task 14 and update of header

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,6 +1,6 @@
 #include "binary_trees.h"
 /**
- * binary_tree_height - measures the height of the tree
+ * binary_tree_height_1 - measures the height of the tree
  * @tree: pointer to the root node of the tree
  * Return : Balance factor or 0
  */
@@ -24,11 +24,11 @@ int binary_tree_height_1(const binary_tree_t *tree)
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
-	 int Rheight, Lheight;
-	 
-	 if (!tree)
-		 return (0);
-	 Rheight = binary_tree_height_1(tree->right);
-	 Lheight = binary_tree_height_1(tree->left);
-	 return (Lheight - Rheight);
+	int Rheight, Lheight;
+	
+	if (!tree)
+		return (0);
+	Rheight = binary_tree_height_1(tree->right);
+	Lheight = binary_tree_height_1(tree->left);
+	return (Lheight - Rheight);
 }
