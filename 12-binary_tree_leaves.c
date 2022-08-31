db86af9d6d4c42edc2ed3e0b95a0b68262191e6d
tizihoxha <tizihoxha@gmail.com>
2022-08-31 06:17:53 -0700

    added task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -9,6 +9,17 @@
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
 {
+	size_t leaves_r, leaves_l, leaves;
+
 	if (tree == NULL)
 		return (0);
+	if (tree != NULL)
+	{
+		leaves_l = binary_tree_leaves(tree->left) + 1;
+		leaves_r = binary_tree_leaves(tree->right) + 1;
+		leaves = leaves_l + leaves_r;
+	}
+	return (leaves);
+}
+
 
