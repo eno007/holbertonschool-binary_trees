d1d1014dc016a5d6a709bae43d75e0a961757562
tizihoxha <tizihoxha@gmail.com>
2022-09-01 02:33:25 -0700

    fixed betty

--- 100-binary_trees_ancestor.c
+++ 100-binary_trees_ancestor.c
@@ -7,11 +7,11 @@
  */
 size_t binary_tree_depth(const binary_tree_t *tree)
 {
-        if (tree == NULL)
-                return (0);
-        if (tree->parent)
-                return (binary_tree_depth(tree->parent) + 1);
-        return (0);
+	if (tree == NULL)
+		return (0);
+	if (tree->parent)
+		return (binary_tree_depth(tree->parent) + 1);
+	return (0);
 }
 
 /**
