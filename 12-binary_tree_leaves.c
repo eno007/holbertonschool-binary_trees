59876414810e4cf19a90695cf2dc3d62acf76ff5
tizihoxha <tizihoxha@gmail.com>
2022-09-01 00:37:11 -0700

    added task 12

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -1,10 +1,9 @@
 #include "binary_trees.h"
 
 /**
- *
- *
- *
- *
+ * binary_tree_leaves - Function that counts the leaves in a binary tree
+ * @tree: pointeer to the root node
+ * Return: number of leaves
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
